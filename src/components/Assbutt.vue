2ec17b3775ed5af22f56568572e3ba97c49299e9
drewwills <wills.drew@gmail.com>
2019-07-28 10:45:43 +0100

    Import phaser into our component and initialize a blank game/canvas

--- src/components/Assbutt.vue
+++ src/components/Assbutt.vue
@@ -1,32 +1,48 @@
 <template>
   <div class="assbutt">
+
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
+import phaser from 'phaser';
+
 export default {
   name: 'Assbutt',
   props: {
     msg: String
+  },
+  created() {
+
+    const preload = function() {
+    }
+
+    const create = function() {
+    }
+
+    const update = function() {
+    }
+
+
+    var config = {
+      type: Phaser.AUTO,
+      width: 600,
+      height: 800,
+      scene: {
+        preload: preload,
+        create: create,
+        update: update
+      }
+    };
+    var game = new Phaser.Game(config);
+
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
